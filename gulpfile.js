02478c47e080331107088c13a438e33870b5b35f
Ryan Coppolo <r@coppolo.com>
2014-03-18 17:23:29 -0700

    Adds gulp-react, probably isn't working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sass = require('gulp-react');
 
 gulp.task('styles', function () {
   gulp.src('./styles/main.scss')
   .pipe(sass())
   .pipe(gulp.dest('./build/styles'));
+});
+
+gulp.task('scripts', function () {
+  gulp.src('./scripts/*.jsx')
+  .pipe(react())
+  .pipe(gulp.dest('./build/scripts'));
 });
 
 gulp.task('default', ['styles']);
